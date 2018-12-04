b0ae041c4056b72fada96c60fba26e96502a4622
BlockCat <zinoonomiwo@gmail.com>
2018-12-04 20:33:34 +0100

    finish sweepline

--- src/day3.rs
+++ src/day3.rs
@@ -46,7 +46,7 @@ impl FromStr for Area {
     }
 }
 
-#[derive(Eq)]
+#[derive(Eq, PartialEq, Debug)]
 enum SweepEvent<'a> {
     StartArea(&'a Area, i32),
     EndArea(&'a Area, i32)
@@ -73,7 +73,7 @@ impl<'a> std::cmp::PartialOrd for SweepEvent<'a> {
         Some(self.cmp(other))
     }
 }
-
+/*
 impl<'a> std::cmp::PartialEq for SweepEvent<'a> {
 
     fn eq(&self, other: &Self) -> bool {
@@ -88,27 +88,27 @@ impl<'a> std::cmp::PartialEq for SweepEvent<'a> {
 
         sy == oy
     }
-}
+}*/
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq)]
 enum HorizontalSwipe {
-    Start(i32),
-    End(i32)
+    Start(i32, i32),
+    End(i32, i32)
 }
 
 impl std::cmp::Ord for HorizontalSwipe {
 
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         let sy = match self {
-            HorizontalSwipe::Start(y) => y,
-            HorizontalSwipe::End(y) => y
+            HorizontalSwipe::Start(id, y) => (y, 0, id),
+            HorizontalSwipe::End(id, y) => (y, 1, id)
         };
-        let oy = match self {
-            HorizontalSwipe::Start(y) => y,
-            HorizontalSwipe::End(y) => y
+        let oy = match other {
+            HorizontalSwipe::Start(id, y) => (y, 0, id),
+            HorizontalSwipe::End(id, y) => (y, 1, id)
         };
 
-        sy.cmp(oy)
+        sy.cmp(&oy)
     }
 }
 
@@ -119,22 +119,17 @@ impl std::cmp::PartialOrd for HorizontalSwipe {
     }
 }
 
-/*
+
 impl std::cmp::PartialEq for HorizontalSwipe {
 
     fn eq(&self, other: &Self) -> bool {
-        let sy = match self {
-            HorizontalSwipe::Start(y) => y,
-            HorizontalSwipe::End(y) => y
-        };
-        let oy = match self {
-            HorizontalSwipe::Start(y) => y,
-            HorizontalSwipe::End(y) => y
-        };
-
-        sy == oy
+        match (self, other) {
+            (HorizontalSwipe::Start(a, _), HorizontalSwipe::Start(b, _)) => a == b,
+            (HorizontalSwipe::End(a, _),HorizontalSwipe::End(b, _)) => a==b,
+            _ => false
+        }
     }
-}*/
+}
 
 
 pub fn execute_exercises() {        
@@ -148,40 +143,65 @@ fn read_input() -> Vec<Area> {
 
 fn exercise_1_sl(input: Vec<Area>) -> i32 {
     
-    let event_vector: Vec<SweepEvent> = input.iter()
+    let mut event_vector: Vec<SweepEvent> = input.iter()
         .map(|s| SweepEvent::StartArea(s, s.y))        
         .chain(input.iter().map(|s| SweepEvent::EndArea(s, s.y + s.height)))
-        .collect();    
+        .collect();
 
-    let min_heap = BinaryHeap::from(event_vector);
-
-    let mut minutes = 0i32;
-    let mut previous_y = 0;
-    let mut previous_overlapped = 0;
+    event_vector.sort();
     let mut state: BTreeSet<HorizontalSwipe> = BTreeSet::new(); // Should be some ordered lists of
 
-    min_heap.iter().fold((0i32, 0i32, 0i32), |(prev_y, prev_overlap, result), event| {
+    event_vector.iter().fold((0i32, 0i32, 0i32), |(prev_y, prev_overlap, result), event| {
         match event {
             SweepEvent::StartArea(b, y) => {
-                let n_result = result + (y - previous_y) * previous_overlapped; // Increment minutes with overlapped
+                let n_result = result + (y - prev_y) * prev_overlap; // Increment minutes with overlapped
                 // Add x and x+width to state.
-                state.insert(HorizontalSwipe::Start(b.x));
-                state.insert(HorizontalSwipe::End(b.x + b.width));
+                state.insert(HorizontalSwipe::Start(b.id, b.x));                
+                state.insert(HorizontalSwipe::End(b.id, b.x + b.width));                
+                
                 // Calculate overlapping
-                let overlapped = 0;
+                let overlapped = count_overlapped(&state);
                 (*y, overlapped, n_result)
             },
             SweepEvent::EndArea(b, y) => {
-                let n_result = result + (y - previous_y) * previous_overlapped; // Increment minutes with overlapped
+                let n_result = result + (y - prev_y) * prev_overlap; // Increment minutes with overlapped
                 // Remove x and x+width to state.
-                state.remove(&HorizontalSwipe::Start(b.x));
-                state.remove(&HorizontalSwipe::End(b.x + b.width));
+                state.remove(&HorizontalSwipe::Start(b.id, b.x));
+                state.remove(&HorizontalSwipe::End(b.id, b.x + b.width));
+                
                 // Calculate overlapping
-                let overlapped = 0;
+                let overlapped = count_overlapped(&state);
                 (*y, overlapped, n_result)
             }
         }        
     }).2
+}
+
+fn count_overlapped(line: &BTreeSet<HorizontalSwipe>) -> i32 {
+
+    line.iter().fold((0i32, 0i32, 0i32), |(overlap, prev_x, counter), event| {        
+        match event {
+            HorizontalSwipe::Start(_, x) => {
+                let increase = if counter > 1 {
+                    *x - prev_x
+                } else {
+                    0
+                };
+
+                (overlap + increase , *x, counter + 1)
+            },
+            HorizontalSwipe::End(_, x) => {
+                let increase = if counter > 1 {
+                    *x - prev_x
+                } else {
+                    0
+                };
+
+                (overlap + increase , *x, counter - 1)
+            }
+        }        
+    }).0
+    
 }
 
 fn exercise_1(input: Vec<Area>) -> i32 {    
@@ -221,6 +241,23 @@ mod tests {
     }
 
     #[test]
+    fn d3_ex1_s3() {
+        assert_eq!(exercise_1(read_input()), 118322);
+    }
+
+    #[test]
+    fn d3_ex1_s4() {
+        let inputs = vec!(Area::new(1, 1, 3, 4, 4), Area::new(2, 3, 1, 4, 4), Area::new(3, 5, 5, 2, 2));
+        assert_eq!(exercise_1_sl(inputs), 4);
+    }
+
+
+    #[test]
+    fn d3_ex1_s5() {
+        assert_eq!(exercise_1_sl(read_input()), 118322);
+    }
+
+    #[test]
     fn d3_ex2_s1() {
         let inputs = vec!(Area::new(1, 1, 3, 4, 4), Area::new(2, 3, 1, 4, 4), Area::new(3, 5, 5, 2, 2));
         assert_eq!(exercise_2(inputs), 3);
@@ -229,15 +266,20 @@ mod tests {
     #[bench]
     fn d3_read(b: &mut Bencher) {
         b.iter(|| read_input());
-    }
+    }   
 
     #[bench]
+    fn d3_ex2_bench(b: &mut Bencher) {
+        b.iter(|| exercise_2(read_input()));
+    }
+
+     #[bench]
     fn d3_ex1_bench(b: &mut Bencher) {
         b.iter(|| exercise_1(read_input()));
     }
 
     #[bench]
-    fn d3_ex2_bench(b: &mut Bencher) {
-        b.iter(|| exercise_2(read_input()));
+    fn d3_ex1_bench_sl(b: &mut Bencher) {
+        b.iter(|| exercise_1_sl(read_input()));
     }
 }
