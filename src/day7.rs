8ae0d84db54295e790a00f00a3f740c1a7c34e29
BlockCat <zinoonomiwo@gmail.com>
2018-12-07 08:39:24 +0100

    day 7 part 2, not wokring

--- src/day7.rs
+++ src/day7.rs
@@ -1,6 +1,7 @@
 
 pub fn execute_exercises() {
     println!("Order: {}", exercise_1(read_input()));
+    println!("parallel time: {}", exercise_2(read_input(), 5, 60)); //1266 is too high
 }
 
 fn read_input() -> Vec<(char, char)> {
@@ -80,8 +81,74 @@ fn exercise_1(input: Vec<(char, char)>) -> String {
     seen.iter().collect::<String>()
 }
 
-fn exercise_2(input: Vec<(char, char)>, workers: u8, seconds_per_step: u8) -> i32 {
+fn exercise_2(input: Vec<(char, char)>, workers: u8, seconds_per_step: i32) -> i32 {
+    use std::collections::BinaryHeap;
+    use std::collections::HashMap;
+
+    let mut steps: Vec<Node> = (b'A'..(b'Z'+1)).map(|c| {
+        Node {
+            value: c as char,
+            next: vec!()
+        }
+    }).collect();
+
+    let mut constraints = HashMap::new();
+
+    for (prec, todo) in input.iter() {        
+        steps[(*prec as u8 - b'A')  as usize].next.push(*todo);
+        constraints.entry(*prec).or_insert(0);
+        let cons = constraints.entry(*todo).or_insert(0);
+        *cons += 1;        
+        //constraints[(*todo as u8- b'A') as usize] += 1;
+    }
     
+    let mut heap = BinaryHeap::new();
+    // Find all chars that have no constraints
+    for (i, c) in constraints.iter().filter(|(_, c)| **c == 0) {        
+        heap.push(&steps[(*i as u8 - b'A') as usize]);
+    }
+
+    let mut event_heap = BinaryHeap::new();
+    let mut working: Vec<bool> = (0..workers).map(|x| false).collect();
+    let mut time = 0;
+    for worker in 0..workers { // Take initial jobs
+        while !heap.is_empty() {
+            let node = heap.pop().unwrap();
+            let time = seconds_per_step + (node.value as u8 - b'A' + 1) as i32;
+            event_heap.push((-time, worker, node));
+            working[worker as usize] = true;
+        }
+    }
+
+    while !event_heap.is_empty() {
+        let (t, worker, node) = event_heap.pop().unwrap();
+        time = -t; //We don't have min heaps
+        println!("{}", time);
+
+        for next_node in node.next.iter() {
+            //constraints[((*next_node as u8) - b'A') as usize] -= 1;
+            let precs = constraints.entry(*next_node).or_insert(0);
+            //let precs = constraints[((*next_node as u8) - b'A') as usize];
+            *precs -= 1;
+            if *precs == 0 {
+                heap.push(&steps[(*next_node as u8 - b'A')  as usize]);
+            }
+        }
+
+        working[worker as usize] = false;        
+        let c = working.iter().enumerate().filter(|(i, is_working)| !**is_working).map(|(a, b)| (a, *b)).collect::<Vec<(usize, bool)>>();
+        for (free_worker, _) in c {
+            while !heap.is_empty() {
+                let node = heap.pop().unwrap();
+                let time = time + seconds_per_step + (node.value as u8 - b'A' + 1) as i32;
+                event_heap.push((-time, free_worker as u8, node));
+                working[free_worker as usize] = true;
+            }
+        }
+        
+    }
+
+    time + 1
 }
 
 #[cfg(test)]
@@ -99,6 +166,19 @@ Step B must be finished before step E can begin.
 Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.";
         assert_eq!(exercise_1(parse_input(input)), "CABDFE");
+    }
+
+    #[test]
+    fn d7_ex2_s1() {
+        let input = r"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+        assert_eq!(exercise_2(parse_input(input), 2, 0), 15);
+    
     }
 
 }
